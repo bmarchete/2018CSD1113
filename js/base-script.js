f79a6ce7365ff7ab888ca1c8d85b3ae3a44036fb
bmarchete <bmarchete@gmail.com>
2018-07-16 19:33:10 -0400

    Fixes navbar content

--- js/base-script.js
+++ js/base-script.js
@@ -36,7 +36,7 @@
 </div>`;
 
   const nav = `
-<a class="navbar-brand" href="#">Site</a>
+
 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor03" aria-controls="navbarColor03"
     aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
@@ -45,7 +45,7 @@
 <div class="collapse navbar-collapse" id="navbarColor03">
     <ul class="navbar-nav mr-auto">
         <li class="nav-item active">
-            <a class="nav-link" href="#">Home
+            <a class="nav-link" href="/">Home
                 <span class="sr-only">(current)</span>
             </a>
         </li>
