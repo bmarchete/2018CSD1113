3496937f3aa1888d6d62b2f9284a3f2e7a3b7770
bmarchete <bmarchete@gmail.com>
2018-07-20 01:41:12 -0400

    Creates register system

--- js/base-script.js
+++ js/base-script.js
@@ -1,7 +1,5 @@
 // generates header and nav
-window.onload = () => {
-  header();
-};
+
 header = () => {
   const header = ` 
 <div class="row">
@@ -30,7 +28,7 @@ header = () => {
             <div class="col">
                 <a href="signup.html">
                     <p class="text-right">Don't you have an account? Sign up Here!</p>
-                </a>
+                </a> <small onclick="logout()">Logout</small>
             </div>
 
         </div>
@@ -72,13 +70,6 @@ header = () => {
   headerEl.innerHTML = header;
 };
 
-setSignUp = (name, email, password) => {
-  const data =
-    (r = JSON.parse(localStorage.getItem("users"))) === null ? [] : r;
-  data.push({ name, email, password });
-  localStorage.setItem("users", JSON.stringify(data));
-};
-
 login = (email, password) => {
   const data =
     (r = JSON.parse(localStorage.getItem("users"))) === null ? false : r;
@@ -97,63 +88,11 @@ login = (email, password) => {
 };
 
 isLogged = () => {
-  return (r = JSON.parse(localStorage.getItem("isLogged"))) === null
-    ? false
-    : r === true
-      ? true
-      : false;
+  const r = JSON.parse(localStorage.getItem("isLogged"));
+  return r === null ? false : true;
 };
 
-logout = () => localStorage.removeItem("isLogged");
-
-setTourismData = () => {
-  const data = [
-    {
-      id: 1,
-      title: "Card title",
-      body:
-        "Some quick example text to build on the card title and make up the bulk of the card's content.",
-      img: "img/mock.jpg"
-    },
-    {
-      id: 2,
-      title: "Card title",
-      body:
-        "Some quick example text to build on the card title and make up the bulk of the card's content.",
-      img: "img/mock.jpg"
-    },
-    {
-      id: 3,
-      title: "Card title",
-      body:
-        "Some quick example text to build on the card title and make up the bulk of the card's content.",
-      img: "img/mock.jpg"
-    }
-  ];
-  localStorage.setItem("tourismThumb", JSON.stringify(data));
-};
-
-fillTourismDataThumbs = () => {
-  const data = JSON.parse(localStorage.getItem("tourismThumb"));
-
-  const dataEl = data
-    .map(t => {
-      return `
-      <div class="col-4  mb-3">
-        <div class="card">
-            <img class="card-img-top" src="${t.img}" alt="Card image cap">
-            <div class="card-body">
-                <h5 class="card-title">${t.title}</h5>
-                <p class="card-text">${t.body}</p>
-                <a href="tourist-details.html?id=${
-                  t.id
-                }" class="btn btn-primary">See more</a>
-            </div>
-        </div>
-      </div>`;
-    })
-    .join("");
-
-  const tourismThumbs = document.querySelector("#tourism-thumbs");
-  tourismThumbs.innerHTML = dataEl;
+logout = () => {
+  localStorage.removeItem("isLogged");
+  window.location.href = "index.html";
 };
---
+++ js/register.js
@@ -0,0 +1,46 @@
+const register = document.querySelector("#register");
+
+register.addEventListener("submit", () => {
+  event.preventDefault();
+
+  let message = signUp(
+    register["name"].value,
+    register["email"].value,
+    register["password"].value,
+    register["cpassword"].value
+  );
+
+  if (message === "ok") {
+    const isLogged = { email: register["email"].value };
+    localStorage.setItem("isLogged", JSON.stringify(isLogged));
+    window.location.href = "index.html";
+  } else {
+    const alert = document.querySelector("#alert");
+    alert.innerHTML = message;
+    alert.classList.remove("d-none");
+  }
+});
+
+validPassword = (password, cpassword) => {
+  return password === cpassword ? true : false;
+};
+
+signUp = (name, email, password, cpassword) => {
+  if (!validPassword(password, cpassword)) {
+    return "Passord and Confirm Password must be the same";
+  }
+
+  const data =
+    (r = JSON.parse(localStorage.getItem("users"))) === null ? [] : r;
+
+  if (data.length > 0) {
+    const value = data.find(v => v.email === email);
+
+    if (value) return "This email alread exists";
+  }
+
+  data.push({ name, email, password });
+  localStorage.setItem("users", JSON.stringify(data));
+
+  return "ok";
+};
