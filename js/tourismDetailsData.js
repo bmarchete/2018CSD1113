882a073ea4dfa60eba748251b18f1005c6d31300
Navjitt <41308897+Navjitt@users.noreply.github.com>
2018-07-26 13:07:15 -0400

    Update tourismDetailsData.js

--- js/tourismDetailsData.js
+++ js/tourismDetailsData.js
@@ -5,7 +5,35 @@ const menu = [
   },
   {
     id: 2,
-    title: "Second link"
+    title: "Royal Ontario Museum"
+  },
+  {
+    id: 3,
+    title: "Ripley aquarium"
+  },
+  {
+    id: 4,
+    title: "Casa Loma"
+  },
+  {
+    id: 5,
+    title: "CN Tower"
+  },
+  {
+    id: 6,
+    title: "Toronto Zoo"
+  },
+  {
+    id: 7,
+    title: "Spadina Museum"
+  },
+  {
+    id: 8,
+    title: "Fort York"
+  },
+  {
+    id: 9,
+    title: "Bluffers Beach"
   }
 ];
 
@@ -29,6 +57,111 @@ const data = [
   {
     id: 2, // needs to match with touristData.js information
     title: `Royal Ontario Museum`,
+    body: `The Royal Ontario Museum was formally established on 16 April 1912, and was jointly governed by the Government of Ontario and the University of Toronto. Its first assets were transferred from the University and the provincial Department of Education, coming from its predecessor the Museum of Natural History and Fine Arts at the Toronto Normal School. On 19 March 1914, at 3:00 pm, the Duke of Connaught, also the Governor General of Canada, officially opened the Royal Ontario Museum to the public. With more than six million items and forty galleries, the museum's diverse collections of world culture and natural history contribute to its international reputation. The museum contains notable collections of dinosaurs, minerals and meteorites, Near Eastern and African art, Art of East Asia, European history, and Canadian history. It houses the world's largest collection of fossils from the Burgess Shalewith more than 150,000 specimens. The museum also contains an extensive collection of design and fine arts, including clothing, interior, and product design, especially Art Deco.`,
+    itinerary: `Some information about the itinerary here`,
+    cost: `Information about costs here`,
+    location: `Put the address here`,
+    map: `put the iframe for map here`,
+    thingsToDo: `Put here what can we do there`,
+    img: [
+      "img/casa_loma_1.jpg",
+      "img/casa_loma_1.jpg" // put all images here separated by comma
+    ]
+  },
+   {
+    id: 3, // needs to match with touristData.js information
+    title: `Ripley Aquarium`,
+    body: `The aquarium building features a multi-faceted shell clad in large aluminum panels, with the roof of the main entrance that "gives the illusion of the earth’s crust peeling away to reveal a window into the aquatic world." There are also coloured surfaces that juxtapose reflective aluminum soffits. The building was designed with special shielding so that Sharks would not be disturbed by its electrical systems.The Ripley aquarium holds 5.7 million litres (1.5 million gallons) of water and shows marine and freshwater habitats from around the world. The aquarium is organized into ten galleries: Canadian Waters, Rainbow Reef, Dangerous Lagoon, Discovery Centre, The Gallery, Ray Bay, Swarm: Nature by Numbers, Planet Jellies, Life Support Systems and the Shoreline Gallery. It is home to more than 20,000 animals.`,
+    itinerary: `Some information about the itinerary here`,
+    cost: `Information about costs here`,
+    location: `Put the address here`,
+    map: `put the iframe for map here`,
+    thingsToDo: `Put here what can we do there`,
+    img: [
+      "img/casa_loma_1.jpg",
+      "img/casa_loma_1.jpg" // put all images here separated by comma
+    ]
+  },
+  
+   {
+    id: 4, // needs to match with touristData.js information
+    title: `Casa Loma`,
+    body: `Spectacular  architecture and modern technology classified Casa Loma as one of the marvels of Toronto in the early 1900s. The castle exhibited the benefits of electric power, which was very new to North America at that time. It had an elevator, which was the first of its kind in a private home in North America, and the castle’s ovens were so big that each one could cook an entire ox! The mansion was fitted with a central vacuuming system and had its own telephone exchange with so many telephones that the castle’s operator handled more daily calls from the castle itself than operators from the entire city of Toronto. The furniture of the Oak Room of Casa Loma was designed by the same company that furnished the Titanic. Casa Loma also was equipped with several secret passages and stairwells, the most interesting of which was an 800-foot-long tunnel that connected the castle to the horse stables. The stables themselves were a stunning wonder, with herringbone tile floors, carved mahogany walls and cast iron panels.`,
+    itinerary: `Some information about the itinerary here`,
+    cost: `Information about costs here`,
+    location: `Put the address here`,
+    map: `<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d11541.795129226131!2d-79.41688202202201!3d43.680432584251534!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x882b33622b6bf9dd%3A0x589f556a98f282b2!2sCasa+Loma%2C+Toronto%2C+ON!5e0!3m2!1sen!2sca!4v1532397614934" width="400" height="300" frameborder="0" style="border:0" allowfullscreen></iframe>`,
+    thingsToDo: `Put here what can we do there`,
+    img: [
+      "img/casa_loma_1.jpg",
+      "img/casa_loma_1.jpg" // put all images here separated by comma
+    ]
+  },
+  
+   {
+    id: 5, // needs to match with touristData.js information
+    title: `CN Tower`,
+    body: `The CN Tower opened to the public on June 26, 1976.The construction costs of approximately CA$63 million ($260 million in 2016 dollars) were repaid in fifteen years. Canadian National Railway sold the tower prior to taking the company private in 1995, when it decided to divest all operations not directly related to its core freight shipping businesses.
+From the mid-1970s to the mid-1980s, the CN Tower was practically the only development along Front Street West; it was still possible to see Lake Ontario from the foot of the CN Tower due to the expansive parking lots and lack of development in the area at the time. As the area around the tower was developed, particularly with the completion of the Metro Toronto Convention Centre in 1984 and the SkyDome in 1989 (renamed Rogers Centre in 2005), the former Railway Lands were redeveloped and the tower became the centre of a newly developing entertainment area. Access was greatly improved with the construction of the SkyWalk in 1989, which connected the tower and SkyDome to the nearby Union Station railway and subway station, and, in turn, to the city's PATH underground pedestrian system. By the mid-1990s, it was the centre of a thriving tourist district. The entire area continues to be an area of intense building, notably a boom in condominiumconstruction in the first two decades of the 21st century, as well as the 2013 opening of the Ripley's Aquarium by the base of the tower.`,
+    itinerary: `Some information about the itinerary here`,
+    cost: `Information about costs here`,
+    location: `301 Front St W, Toronto, ON M5V 2T6`,
+    map: `<iframe src="https://www.google.ca/maps/dir/''/cn+tower/@43.6425448,-79.4570966,12z/data=!4m8!4m7!1m0!1m5!1m1!1s0x882b34d68bf33a9b:0x15edd8c4de1c7581!2m2!1d-79.3870568!2d43.6425662" width="400" height="300" frameborder="0" style="border:0" allowfullscreen></iframe>`,
+    thingsToDo: `Put here what can we do there`,
+    img: [
+      "img/casa_loma_1.jpg",
+      "img/casa_loma_1.jpg" // put all images here separated by comma
+    ]
+  },
+  
+   {
+    id: 6, // needs to match with touristData.js information
+    title: `Toronto Zoo`,
+    body: ``,
+    itinerary: `Some information about the itinerary here`,
+    cost: `Information about costs here`,
+    location: `Put the address here`,
+    map: `put the iframe for map here`,
+    thingsToDo: `Put here what can we do there`,
+    img: [
+      "img/casa_loma_1.jpg",
+      "img/casa_loma_1.jpg" // put all images here separated by comma
+    ]
+  },
+  
+   {
+    id: 7, // needs to match with touristData.js information
+    title: `Spadina Museum`,
+    body: ``,
+    itinerary: `Some information about the itinerary here`,
+    cost: `Information about costs here`,
+    location: `Put the address here`,
+    map: `put the iframe for map here`,
+    thingsToDo: `Put here what can we do there`,
+    img: [
+      "img/casa_loma_1.jpg",
+      "img/casa_loma_1.jpg" // put all images here separated by comma
+    ]
+  },
+  
+   {
+    id: 8, // needs to match with touristData.js information
+    title: `Fort York`,
+    body: ``,
+    itinerary: `Some information about the itinerary here`,
+    cost: `Information about costs here`,
+    location: `Put the address here`,
+    map: `put the iframe for map here`,
+    thingsToDo: `Put here what can we do there`,
+    img: [
+      "img/casa_loma_1.jpg",
+      "img/casa_loma_1.jpg" // put all images here separated by comma
+    ]
+  },
+  
+   {
+    id: 9, // needs to match with touristData.js information
+    title: `Bluffers Beach`,
     body: ``,
     itinerary: `Some information about the itinerary here`,
     cost: `Information about costs here`,
